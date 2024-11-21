08d8fa7289bb765d5bbe7399756ecec453745071
d-sava <dimitrijevicsava8@gmail.com>
2024-11-21 22:35:44 +0100

    modified:   Main.class 	modified:   Main.java 	modified:   SearchAlgorithms.class 	modified:   SearchAlgorithms.java

--- Main.java
+++ Main.java
@@ -1,21 +1,77 @@
 public class Main{
     public static void main(String[] args) {
-        //testing linear search
-        int[] array = {1, 5, 2, 4};
+        long start, end, result;
+        int index;
 
-        System.out.println("\nLinear search: \n");
-        System.out.println("Index of '5' is " +SearchAlgorithms.linearSearch(array, 5));
-        System.out.println("Index of '4' is " +SearchAlgorithms.linearSearch(array, 4));
-        System.out.println("Index of '2' is " +SearchAlgorithms.linearSearch(array, 2));
-        System.out.println("Index of '1' is " +SearchAlgorithms.linearSearch(array, 1));
+        //generating random datasets: 10, 100, 1000, 10000
+        int[] randomDataset_10 = generateRandomArray(10);
+        int[] randomDataset_100 = generateRandomArray(100);
+        int[] randomDataset_1000 = generateRandomArray(1000);
+        int[] randomDataset_10000 = generateRandomArray(10000);
+
+        //generating sorted datasets: 10, 100, 1000, 10000
+        int[] sortedDataset_10 = generateSortedArray(10);
+        int[] sortedDataset_100 = generateSortedArray(100);
+        int[] sortedDataset_1000 = generateSortedArray(1000);
+        int[] sortedDataset_10000 = generateSortedArray(10000);
+
+        //testing linear search: array of 10 elements
+        start = System.currentTimeMillis();
+        index = SearchAlgorithms.linearSearch(randomDataset_10, 5);
+        end = System.currentTimeMillis();
+        calculateAlgorithmEfficiency(start, end, "Linear Search", 10);
+
+         //testing linear search: array of 100 elements
+         start = System.currentTimeMillis();
+         index = SearchAlgorithms.linearSearch(randomDataset_100, 50);
+         end = System.currentTimeMillis();
+         calculateAlgorithmEfficiency(start, end, "Linear Search", 100);
+
+         //testing linear search: array of 1000 elements
+         start = System.currentTimeMillis();
+         index = SearchAlgorithms.linearSearch(randomDataset_1000, 30);
+         end = System.currentTimeMillis();
+         calculateAlgorithmEfficiency(start, end, "Linear Search", 1000);
+
+         //testing linear search: array of 10000 elements
+         start = System.currentTimeMillis();
+         index = SearchAlgorithms.linearSearch(randomDataset_10000, 70);
+         end = System.currentTimeMillis();
+         calculateAlgorithmEfficiency(start, end, "Linear Search", 10000);
 
         //testing binary search
-        int[] sortedArray = {1, 2, 3, 4};
 
-        System.out.println("\nBinary search: \n");
-        System.out.println("Index of '2' is " +SearchAlgorithms.binarySearch(sortedArray, 2));
-        System.out.println("Index of '4' is " +SearchAlgorithms.binarySearch(sortedArray, 4));
-        System.out.println("Index of '3' is " +SearchAlgorithms.binarySearch(sortedArray, 3));
-        System.out.println("Index of '1' is " +SearchAlgorithms.binarySearch(sortedArray, 1));
+    }
+
+    public static int[] generateRandomArray(int limit){
+        int[] array = new int[limit];
+
+        for(int i = 0; i < array.length; i++){
+            array[i] = (int)(Math.random() * 100);
+        }
+
+        return array; 
+    }
+
+    public static int[] generateSortedArray(int limit){
+        int[] array = new int[limit];
+
+        for(int i = 0; i < array.length; i++){
+            array[i] = i;
+        }
+
+        return array;
+    }
+
+    public static void calculateAlgorithmEfficiency(long start, long end, String algorithm, int input){
+        long result = end - start;
+        System.out.println("\n"+algorithm + " execution time for dataset of " +input+ " elements is: " +result + "ms");
+
+        if(result < 1)
+            System.out.println("Great performance.");
+        else if(result < 4)
+            System.out.println("Good performance.");
+        else
+            System.out.println("Bad performance.\n");
     }
 }
--- SearchAlgorithms.java
+++ SearchAlgorithms.java
@@ -14,26 +14,25 @@ public class SearchAlgorithms {
 
     public static int binarySearch(int[] sortedArray, int target){
         //O(log2N) complexity
-
         int high = sortedArray.length-1; 
         int low = 0;
-        int value = 0;
+        int value;
         int middle;
 
         while(low<=high){
 
-            middle = (high+low)/2;
-            value = sortedArray[middle];
+            middle = (high+low)/2;//Find the middle index
+            value = sortedArray[middle];//Get the value at the middle index
 
             if(target > value)
-                low = middle-1;
+                low = middle+1;//Move low up
             else if(target < value)
-                high = middle+1;
+                high = middle-1;//Move high down
             else
-                return value;
+                return middle;//Return the index if target is found
 
         }
 
-        return -1;
-    }
+        return -1;//Return -1 if the target is not found
+    } 
 }
